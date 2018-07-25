9805e9f05e03c7bec00bad5c95086b4fbfab6357
szabolcsGyorki <sz.gyorki@gmail.com>
2018-07-25 11:43:01 +0200

    Implemented methods to read lines and test it

--- tests/com/codecool/file_part_reader/FilePartReaderTest.java
+++ tests/com/codecool/file_part_reader/FilePartReaderTest.java
@@ -11,7 +11,7 @@ class FilePartReaderTest {
     private String TEST_FILE_PATH = "files/testFile.txt";
 
     @Test
-    void testIfTheFileIsReadCorrectly() throws FileNotFoundException {
+    void When_FilePathIsCorrect_Expect_ContentReturnedAsString() throws FileNotFoundException {
         String originalContent = "This assignment we will work lol with files\n" +
                 "because we cannot have enough file readers\n" +
                 "devoved we are oh yeah\n" +
@@ -19,7 +19,27 @@ class FilePartReaderTest {
                 "Who can be the first racer in this evening";
         FilePartReader reader = new FilePartReader();
         reader.setup(TEST_FILE_PATH, 1, 5);
-        assertEquals(originalContent, reader.readLines());
+        assertEquals(originalContent, reader.read());
+    }
+
+    @Test
+    void When_InputsAreCorrect_Expect_GivenLinesAreReturned() throws FileNotFoundException {
+        String expected = "because we cannot have enough file readers\n" +
+                "devoved we are oh yeah";
+        FilePartReader reader = new FilePartReader();
+        reader.setup(TEST_FILE_PATH, 2, 3);
+        assertEquals(expected, reader.readLines());
+
+        expected = "This assignment we will work lol with files";
+        reader.setup(TEST_FILE_PATH, 1, 1);
+        assertEquals(expected, reader.readLines());
+
+        expected = "because we cannot have enough file readers\n" +
+                "devoved we are oh yeah\n" +
+                "The madam is in charge here\n" +
+                "Who can be the first racer in this evening";
+        reader.setup(TEST_FILE_PATH, 2, 5);
+        assertEquals(expected, reader.readLines());
     }
 
 }
