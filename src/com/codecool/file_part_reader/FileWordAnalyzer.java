40d4de28281afd29cccb31c7ddbe73857a69d097
szabolcsGyorki <sz.gyorki@gmail.com>
2018-07-25 15:25:27 +0200

    Implemented FileWordAnalyzer methods

--- src/com/codecool/file_part_reader/FileWordAnalyzer.java
+++ src/com/codecool/file_part_reader/FileWordAnalyzer.java
@@ -1,6 +1,8 @@
 package com.codecool.file_part_reader;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class FileWordAnalyzer {
 
@@ -10,19 +12,28 @@ public class FileWordAnalyzer {
         this.reader = reader;
     }
 
-//    public ArrayList<String> wordsByABC() {
-//        reader.readLines();
-//        return null;
-//    }
-//
-//    public ArrayList<String> wordsContainingSubString(String subString) {
-//        reader.readLines();
-//        return null;
-//    }
-//
-//    public ArrayList<String > wordsArePalindrome() {
-//        reader.readLines();
-//        return null;
-//    }
+    private ArrayList<String> readLines() {
+        return new ArrayList<>(Arrays.asList(reader.readLines().split("\n\\\\")));
+    }
+
+    public ArrayList<String> wordsByABC() {
+        ArrayList<String> lines = readLines();
+        lines.sort(String::compareToIgnoreCase);
+        return lines;
+    }
+
+    public ArrayList<String> wordsContainingSubString(String subString) {
+        ArrayList<String> lines = readLines();
+        return lines.stream()
+                .filter(s -> s.contains(subString))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public ArrayList<String > wordsArePalindrome() {
+        ArrayList<String> lines = readLines();
+        return lines.stream()
+                .filter(s -> s.equals(new StringBuilder(s).reverse().toString()))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
 
 }
