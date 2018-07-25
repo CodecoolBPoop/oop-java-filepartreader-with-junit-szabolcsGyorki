4ad2b400d821e61af746d0e08f1581612622b8f6
szabolcsGyorki <sz.gyorki@gmail.com>
2018-07-25 11:01:54 +0200

    Implemented methods to read a file and test it

---
+++ src/com/codecool/file_part_reader/FileWordAnalyzer.java
@@ -0,0 +1,28 @@
+package com.codecool.file_part_reader;
+
+import java.util.ArrayList;
+
+public class FileWordAnalyzer {
+
+    private FilePartReader reader;
+
+    public FileWordAnalyzer(FilePartReader reader) {
+        this.reader = reader;
+    }
+
+//    public ArrayList<String> wordsByABC() {
+//        reader.readLines();
+//        return null;
+//    }
+//
+//    public ArrayList<String> wordsContainingSubString(String subString) {
+//        reader.readLines();
+//        return null;
+//    }
+//
+//    public ArrayList<String > wordsArePalindrome() {
+//        reader.readLines();
+//        return null;
+//    }
+
+}
