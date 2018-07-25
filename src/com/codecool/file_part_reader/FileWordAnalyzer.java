88acfdd761d32c11335d3386a2efd58a8aa2ee00
szabolcsGyorki <sz.gyorki@gmail.com>
2018-07-25 15:51:21 +0200

    Test for ABC sorting implemented

--- src/com/codecool/file_part_reader/FileWordAnalyzer.java
+++ src/com/codecool/file_part_reader/FileWordAnalyzer.java
@@ -4,32 +4,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class FileWordAnalyzer {
+class FileWordAnalyzer {
 
     private FilePartReader reader;
 
-    public FileWordAnalyzer(FilePartReader reader) {
+    FileWordAnalyzer(FilePartReader reader) {
         this.reader = reader;
     }
 
     private ArrayList<String> readLines() {
-        return new ArrayList<>(Arrays.asList(reader.readLines().split("\n\\\\")));
+        return new ArrayList<>(Arrays.asList(reader.readLines().split("\\s")));
     }
 
-    public ArrayList<String> wordsByABC() {
+    ArrayList<String> wordsByABC() {
         ArrayList<String> lines = readLines();
         lines.sort(String::compareToIgnoreCase);
         return lines;
     }
 
-    public ArrayList<String> wordsContainingSubString(String subString) {
+    ArrayList<String> wordsContainingSubString(String subString) {
         ArrayList<String> lines = readLines();
         return lines.stream()
                 .filter(s -> s.contains(subString))
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public ArrayList<String > wordsArePalindrome() {
+    ArrayList<String > wordsArePalindrome() {
         ArrayList<String> lines = readLines();
         return lines.stream()
                 .filter(s -> s.equals(new StringBuilder(s).reverse().toString()))
--- tests/com/codecool/file_part_reader/FileWordAnalyzerTest.java
+++ tests/com/codecool/file_part_reader/FileWordAnalyzerTest.java
@@ -1,7 +1,41 @@
 package com.codecool.file_part_reader;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileWordAnalyzerTest {
+
+    private String TEST_FILE_PATH = "files/testFile.txt";
+
+    @Test
+    void When_WordsByABCIsCalled_Expect_ReturnWordsAlphabetically() {
+        FilePartReader reader = new FilePartReader();
+        reader.setup(TEST_FILE_PATH, 1, 2);
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("assignment");
+        expected.add("because");
+        expected.add("cannot");
+        expected.add("enough");
+        expected.add("file");
+        expected.add("files");
+        expected.add("have");
+        expected.add("lol");
+        expected.add("readers");
+        expected.add("This");
+        expected.add("we");
+        expected.add("we");
+        expected.add("will");
+        expected.add("with");
+        expected.add("work");
+
+        assertEquals(expected, analyzer.wordsByABC());
+    }
+
+
 
 }
